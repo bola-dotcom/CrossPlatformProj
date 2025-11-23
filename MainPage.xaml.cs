4be7a672a4fa6a3f90ef2dc38c370db3674d61d5
bola-dotcom <G00462097@atu.ie>
2025-11-23 19:19:23 +0000

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,61 +4,67 @@ using System.Text.Json;
 
 namespace Project
 {
+
     public partial class MainPage : ContentPage
     {
         int count = 0;
-        
-        private HttpClient _httpClient;
 
+        private HttpClient _httpClient;
+        private movieViewModel viewModel;
 
 
         public MainPage()
         {
             InitializeComponent();
-            
+
+            viewModel= new movieViewModel();
+            BindingContext = viewModel;
             _httpClient = new HttpClient();
-            if (count == 0)
+           if (count == 0)
             {
-                downloadFile();
+                viewModel.downloadFile();
             }
-        }
-        private async void downloadFile()
-        {
-            //if this is the first time the application is opened
            
-                //gets the link into the response variable
-                var response = await _httpClient.GetAsync("https://raw.githubusercontent.com/DonH-ITS/jsonfiles/refs/heads/main/moviesemoji.json");
-
-
-                if (response == null)
-                {
-
-                }
-                //if the response is successful
-                if (response.IsSuccessStatusCode)
-                {
-                    //reads the content of the json into the text variable
-                    String text = await response.Content.ReadAsStringAsync();
-                    //deserializes the content into the texts variable
-                    var texts = JsonSerializer.Deserialize<List<Root>>(text);
-
-                //each film in the texts list and call it film
-                foreach (var film in texts)
-                    // Console.WriteLine(film.title);
-                    movies.Text= text;
-                }
-            }
         }
-        public class Root
-        {
-            public string title { get; set; }
-            public int year { get; set; }
-            public List<string> genre { get; set; }
-            public string director { get; set; }
-            public double rating { get; set; }
-            public string emoji { get; set; }
-        }
+        /*  private async void downloadFile()
+          {
+              //if this is the first time the application is opened
+
+                  //gets the link into the response variable
+                  var response = await _httpClient.GetAsync("https://raw.githubusercontent.com/DonH-ITS/jsonfiles/refs/heads/main/moviesemoji.json");
+
+
+                  if (response == null)
+                  {
+
+                  }
+                  //if the response is successful
+                  if (response.IsSuccessStatusCode)
+                  {
+                      //reads the content of the json into the text variable
+                      String text = await response.Content.ReadAsStringAsync();
+                      //deserializes the content into the texts variable
+                      var texts = JsonSerializer.Deserialize<List<Root>>(text);
+
+                  //each film in the texts list and call it film
+                  foreach (var film in texts)
+                      // Console.WriteLine(film.title);
+                      movies.Text= text;
+                  }
+              }
+          }
+          public class Root
+          {
+              public string title { get; set; }
+              public int year { get; set; }
+              public List<string> genre { get; set; }
+              public string director { get; set; }
+              public double rating { get; set; }
+              public string emoji { get; set; }
+          }
+        */
 
     }
+}
 
 
